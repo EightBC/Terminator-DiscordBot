c75f3fc8c2599f048d04bcbed45c68cab07f1742
EightBC <89529207838@yandex.ru>
2022-02-14 21:49:36 +0700

    First version of exterminating homeless

--- main.py
+++ main.py
@@ -17,11 +17,43 @@ async def on_message(message):
     if message.author.bot:
         return
 
-    if message.content.startswith('$role'):
-        await message.channel.send(message.author.roles)
+    if not message.content.startswith('$'):
+        if isinstance(message.author, discord.Member) and not len(message.author.roles) == 1:
+            pass
+        else:
+            counter = 0
+            for channel in message.guild.text_channels:
+                async for msg in channel.history(limit=10):
+                    if msg.content == message.content:
+                        counter += 1
+            if counter > 4:
+                bomj = message.author
+                await ban_function(bomj, message)
+                return
+
+
+    # if message.content.startswith('$ban'):
+    #     bomj = message.mentions[0]
+    #     await ban_function(bomj, message)
+    #     return
+
+    if message.content.startswith('$owner'):
+        await message.channel.send(type(message.guild.owner))
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+
+async def ban_function(bomj, message):
+    try:
+        await message.guild.fetch_ban(bomj)
+    except discord.errors.NotFound:
+        if bomj.dm_channel == None:
+            await bomj.create_dm()
+        await bomj.dm_channel.send('Тааа\nШааа\nПиши 8bc#5255 если хочешь вернуться в уличные гонки')
+        await message.channel.send(f'Member {bomj} has been banned. Reason: Spam')
+        await message.guild.ban(bomj, reason="лох", delete_message_days=1)
+
 
 f = open('token.txt', 'r')
 t = f.readline().strip()
